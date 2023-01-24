ea49dfeb2726d0e2944c50bbb88e460f6c52b6d6
abhi shiba <shivarajabhishek@gmail.com>
2023-01-24 08:49:39 +0530

    route

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,20 @@ import { Home } from "./Home";
 import { CheckOut } from "./CheckOut/CheckOut";
 import { NavBar } from "./BookMyShow/NavBar/NavBar";
 import { WishList } from "./WishList/WishList";
+import { Movies } from "./BookMyShow/Movies/Movies";
 
 function App() {
   return (
     <div id="main">
-    {/* <BrowserRouter > */}
-    <HashRouter>
+    <BrowserRouter >
+    {/* <HashRouter> */}
       <Routes>
         <Route path="/" element={<Home />}  />
         <Route path="/checkout" element={<CheckOut/>} />
         <Route path="/wishlist" element={<><NavBar/><WishList/></>} />
       </Routes>
-      </HashRouter>
-    {/* </BrowserRouter> */}
+      {/* </HashRouter> */}
+    </BrowserRouter>
     </div>
   );
 }
